74853e27a40da1fb9b37f9ead21a7163f1df3143
Viktor Papara <eucalypto@mail.de>
2020-11-12 19:33:51 +0100

    Implement part of the solution (#3)
    The print out of the cinema with the selected seat is still not implemented.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,19 +1,27 @@
 package cinema;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Cinema {
 
+    Scanner scanner = new Scanner(System.in);
+    private int rows;
+    private int seatsPerRow;
+    private List<Integer> rowPrices;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number of rows:");
-        int rows = scanner.nextInt();
+        Cinema cinema = new Cinema();
 
-        System.out.println("Enter the number of seats in each row:");
-        int seatsPerRow = scanner.nextInt();
+        cinema.setUp();
+        cinema.calculateRowPrices();
 
-        System.out.println("Total income:\n$" + calculateIncome(rows, seatsPerRow));
+        System.out.println(cinema);
+
+        cinema.getTicketPrice();
+
 
     }
 
@@ -27,5 +35,71 @@ public class Cinema {
         int frontRows = rows / 2;
 
         return frontRows * seatsPerRow * 10 + (rows - frontRows) * seatsPerRow * 8;
+    }
+
+    private void calculateRowPrices() {
+        rowPrices = new ArrayList<>();
+
+        int totalSeats = rows * seatsPerRow;
+
+        if (totalSeats <= 60) {
+            for (int i = 0; i < rows; i++) {
+                rowPrices.add(10);
+            }
+        } else {
+            int frontRows = rows / 2;
+            for (int i = 0; i < frontRows; i++) {
+                rowPrices.add(10);
+            }
+            for (int i = 0; i < rows - frontRows; i++) {
+                rowPrices.add(8);
+            }
+        }
+    }
+
+    private void getTicketPrice() {
+        System.out.println("Enter a row number:");
+        int selectedRow = scanner.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+        int selectedSeat = scanner.nextInt();
+
+        System.out.println("Ticket price: $" + getTicketPrice(selectedRow));
+    }
+
+    private int getTicketPrice(int selectedRow) {
+        return rowPrices.get(selectedRow - 1);
+    }
+
+    private void setUp() {
+
+        System.out.println("Enter the number of rows:");
+        rows = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        seatsPerRow = scanner.nextInt();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+
+        // first 2 lines
+        builder.append("Cinema:\n");
+        builder.append(" ");
+        for (int i = 1; i <= seatsPerRow; i++) {
+            builder.append(" ").append(i);
+        }
+        builder.append("\n");
+
+        // other lines
+        for (int row = 1; row <= rows; row++) {
+            builder.append(row);
+            builder.append(" S".repeat(seatsPerRow));
+            builder.append("\n");
+        }
+
+        return builder.toString();
     }
 }
