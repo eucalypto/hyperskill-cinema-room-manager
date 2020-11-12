e74138dd4ab0cc9d9a8efa3abb7a6e6b38288a24
Viktor Papara <eucalypto@mail.de>
2020-11-12 18:12:09 +0100

    Implement solution for stage 2 (#2)
    that passes the tests :)

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,12 +1,31 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i <= 7; i++) {
-            System.out.println(i + " S S S S S S S S");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int seatsPerRow = scanner.nextInt();
+
+        System.out.println("Total income:\n$" + calculateIncome(rows, seatsPerRow));
+
+    }
+
+    private static int calculateIncome(int rows, int seatsPerRow) {
+        int totalSeats = rows * seatsPerRow;
+
+        if (totalSeats <= 60) {
+            return totalSeats * 10;
         }
+
+        int frontRows = rows / 2;
+
+        return frontRows * seatsPerRow * 10 + (rows - frontRows) * seatsPerRow * 8;
     }
 }
